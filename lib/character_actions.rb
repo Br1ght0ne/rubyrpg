7aa1a5c266f53152fc2d38c55a99e0fb70e166d1
Алексей Филоненко <filalex77@gmail.com>
2016-05-14 15:51:04 +0300

    RubyRPG 0.2 Pre-Alpha
    Добавлено:
    - версия игры при загрузке
    - оружие, предметы, отображение информации про оружие и предметы
    Изменено:
    - имя загрузочного скрипта (rpg.rb => rubyrpg.rb)

--- lib/character_actions.rb
+++ lib/character_actions.rb
@@ -2,4 +2,12 @@ module Fighting
   def attack(attacker, target)
   end
 end
+
+module Items
+  def inspect_items(player)
+    puts "\nDisplaying info for #{player.class}: #{player.name}..."
+    sleep(0.5)
+    puts "\nName: #{player.name}\nLevel: #{player.lvl}\nHP: #{player.hp}\nDamage: #{player.dmg_min}-#{player.dmg_max}\nEvasion: #{player.evasion}\nAccuracy: #{player.accuracy}"
+  end
+end
 # Movement Talking Skills Items Exp Drop
--- lib/modules.rb
+++ lib/modules.rb
@@ -1,5 +1,6 @@
 module TextBlocks
   def start
+    require_relative "build_info"
     puts "\n\n"
     puts "      #######     "
     puts "     #&&&&&&&#    "
@@ -14,13 +15,13 @@ module TextBlocks
     puts "  # #  #    #  #  "
     puts "  #  # #    ####  "
     puts "\n"
-    puts "Welcome to RubyRPG! \n"
+    puts "Welcome to #{$game_name} #{$game_version} #{$game_version_tag}! \n"
   end
 end
 
 module Action
   def get_player_action(player)
-    puts "\nWhat do you want to do?\nd - display information about you | i - inspect your items | m - move to some location | t - TERMINATE GAME"
+    puts "\nWhat do you want to do?\nd - display information about you | i - inspect your items | m - move to some location (ACHTUNG: not yet implemented) | t - TERMINATE GAME"
     user_action = gets.chomp
     case user_action
     when "d"
