6dd977f2aa04abc2ad331c6ba4fe45f6009a405e
Алексей Филоненко <filalex77@gmail.com>
2016-05-16 22:58:22 +0300

    Добавлено зон, изменены коды зон

--- lib/zones.rb
+++ lib/zones.rb
@@ -2,6 +2,14 @@ require_relative "characters"
 class Zone
 end
 
+class ZoneGenerator
+  def initialize
+    @@zones = [Graveyard.new, Forest.new]
+    @generated_zone = @@zones.sample
+  end
+  attr_reader :zones; attr_reader :generated_zone
+end
+
 class StartZone < Zone
   def initialize
     @name = "Start Zone"
@@ -15,7 +23,7 @@ class Graveyard < Zone
   @@graveyard_count = 0
   def initialize
     @@graveyard_count += 1; @graveyard_number = @@graveyard_count
-    @name = "Graveyard"; @code = "G"
+    @name = "Graveyard"; @code = "GR#{@graveyard_number}"
     @desc = "an old graveyard"
     # FIXME: @ghost_appear = true if rand(1..100) <= Ghost.@@appear_chance
     # FIXME: Ghost.new if @ghost_appear == true
@@ -28,19 +36,37 @@ class Forest < Zone
   @@forest_count = 0
   def initialize
     @@forest_count +=1; @forest_number = @@forest_count
-    @name = "Forest"; @code = "F"
+    @name = "Forest"; @code = "FO#{@forest_number}"
     @desc = "a thick forest"
     # FIXME: @ghoul_appear = true if rand(1..100) <=  Ghoul.@@appear_chance
     # FIXME: Ghoul.new if @ghoul_appear == true
   end
-  attr_reader :name; attr_reader :forest_number; attr_reader :code; attr_reader :desc
+  attr_reader :name, :forest_number, :code, :desc
   # FIXME: attr_reader :ghoul_appear
 end
 
-class ZoneGenerator
+class Cave < Zone
+  @@cave_count = 0
   def initialize
-    @@zones = [Graveyard.new, Forest.new]
-    @generated_zone = @@zones.sample
+    @@cave_count +=1; @cave_number = @@cave_count
+    @name = "Cave"; @code = "CV#{@cave_number}"
+    @desc = "a deep cave"
+    # FIXME: @ghoul_appear = true if rand(1..100) <=  Ghoul.@@appear_chance
+    # FIXME: Ghoul.new if @ghoul_appear == true
   end
-  attr_reader :zones; attr_reader :generated_zone
+  attr_reader :name, :cave_number, :code, :desc
+  # FIXME: attr_reader :ghoul_appear
+end
+
+class Castle < Zone
+  @@castle_count = 0
+  def initialize
+    @@castle_count +=1; @castle_number = @@castle_count
+    @name = "Castle"; @code = "CS#{@cave_number}"
+    @desc = "an ancient castle"
+    # FIXME: @ghoul_appear = true if rand(1..100) <=  Ghoul.@@appear_chance
+    # FIXME: Ghoul.new if @ghoul_appear == true
+  end
+  attr_reader :name, :castle_number, :code, :desc
+  # FIXME: attr_reader :ghoul_appear
 end
