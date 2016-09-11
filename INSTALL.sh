0112a99722007655a1e5697fd3da06cc15c2ab16
Alex Filonenko <filalex77@gmail.com>
2016-09-11 15:05:20 +0300

    исправлен скрипт установки

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,12 +4,12 @@ echo "Installing RubyRPG to $ENDPATH ..."
 # if [ -d "$ENDPATH" ]; then
     mkdir -p "$ENDPATH"
 # fi
-mkdir $ENDPATH/lib
+mkdir $ENDPATH/lib &> /dev/null
 cp -rf lib $ENDPATH &> /dev/null
 cp -f CHANGELOG.md $ENDPATH/CHANGELOG.md &> /dev/null
 cp -f LICENSE.txt $ENDPATH/LICENSE.txt &> /dev/null
 cp -f README.md $ENDPATH/README.md &> /dev/null
 cp -f rubyrpg $ENDPATH/rubyrpg &> /dev/null
-echo "Copied successfully."
+echo "Installed successfully."
 echo "You can launch the game by typing:"
-echo "    $HOME/games/rubyrpg"
+echo "    $ENDPATH/rubyrpg"
