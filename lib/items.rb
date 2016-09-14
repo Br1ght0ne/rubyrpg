326ecd72c8bf9433939bc64ab235227adac8f23f
Alex Filonenko <filalex77@gmail.com>
2016-09-15 02:33:45 +0300

    полностью переработаны персонажи, полномасштабный рефакторинг всего

--- lib/items.rb
+++ lib/items.rb
@@ -1,3 +1,56 @@
+class Inventory
+  def initialize
+    @items = [SmallHealthPotion.new]
+  end
+  attr_accessor :items
+end
+
+class Equipment
+  def initialize
+    weapon_init
+    armor_init
+    jewelry_init
+  end
+
+  attr_accessor :weapon,
+                :helmet, :armor, :gloves, :cape, :boots, :leggings,
+                :ring_left, :ring_right, :amulet, :belt
+
+  private
+
+  def weapon_init
+    @weapon = Weapon.new('Handmade Dagger', 2)
+  end
+
+  def armor_init
+    @helmet = Helmet.new('no helmet', 0)
+    @armor = Armor.new('no armor', 0)
+    @gloves = Gloves.new('no gloves', 0)
+    @cape = Cape.new('no cape', 0)
+    @boots = Boots.new('no boots', 0)
+    @leggings = Leggings.new('no leggings', 0)
+  end
+
+  def jewelry_init
+    @ring_left = RingLeft.new('no ring on left hand', '*nothing*', 0)
+    @ring_right = RingRight.new('no ring on right hand', '*nothing*', 0)
+    @amulet = Amulet.new('no amulet', '*nothing*', 0)
+    @belt = Belt.new('no belt', '*nothing*', 0)
+  end
+
+  public
+
+  def list
+    items = [@weapon, @helmet, @armor, @gloves, @cape, @boots,
+             @leggings, @ring_left, @ring_right,
+             @amulet, @belt]
+    items.each do |item|
+      puts "\n#{item.type}: #{item.name} - increases #{item.increase_type} "\
+           "by #{item.increase_value}"
+    end
+  end
+end
+
 class Weapon
   def initialize(name, dmg_increase)
     @type = 'Weapon'
@@ -122,21 +175,26 @@ end
 class HealthPotion < Potion
   def initialize
     @usage = "type #{@code} to use"
-    @isConsumable = true
+    @is_consumable = true
     @desc = "restores your HP by #{@hp_restore}"
   end
 
   def use
     raise if $player.hp == $player.max_hp
-    hp_before_heal = $player.hp
-    $player.hp += @hp_restore
-    $player.hp = $player.max_hp if $player.hp > $player.max_hp
-    healed = $player.hp - hp_before_heal
+    healed = heal
     puts "Restored #{healed} HP."
   rescue
     puts "You can\'t use health potions while you have full HP."
     sleep(1.5)
     $player.inspect_items
+  end
+
+  def heal
+    hp_before_heal = $player.hp
+    $player.hp += @hp_restore
+    $player.hp = $player.max_hp if $player.hp > $player.max_hp
+    healed = $player.hp - hp_before_heal
+    healed
   end
 end
 
@@ -147,7 +205,7 @@ class SmallHealthPotion < HealthPotion
     @hp_restore = 20
     super
   end
-  attr_reader :name, :code, :hp_restore, :desc, :isConsumable, :usage
+  attr_reader :name, :code, :hp_restore, :desc, :is_consumable, :usage
 end
 
 class DroppedItems
@@ -156,11 +214,11 @@ end
 class Junk < DroppedItems
   def initialize
     @usage = 'junk, no use'
-    @isConsumable = true
+    @is_consumable = true
     @desc = 'should be sold in shop'
   end
 
-  attr_reader :usage, :isConsumable, :desc
+  attr_reader :usage, :is_consumable, :desc
 end
 
 class StainedSheet < Junk
